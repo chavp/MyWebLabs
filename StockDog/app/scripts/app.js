b8a07784249b1e024f410cb19b5418e2de346209
chavp <my.parinya@gmail.com>
2016-01-03 00:50:02 +0700

    add rpojects StockDog
    Signed-off-by: chavp <my.parinya@gmail.com>

---
+++ StockDog/app/scripts/app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name stockDogApp
+ * @description
+ * # stockDogApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('stockDogApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
